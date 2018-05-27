e0d505b920460eb210dbec8f184d943c0e44d2be
Lashaunna Samuels <shaunna191@hotmail.com>
2018-05-27 12:54:47 -0400

    Made the about image responsive

--- src/Components/About/AboutStyles.js
+++ src/Components/About/AboutStyles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import woodStack from './img/woodstack.jpg';
+// import woodStack from './img/woodstack.jpg';
 
 export const About = styled.section `
   background: rgb(243, 243, 241);
@@ -10,24 +10,26 @@ export const About = styled.section `
   grid-template-rows: 20vh auto 25vh 25vh;
 `
 export const WoodStack = styled.div `
-background-image: url(${woodStack});
-background-repeat: no-repeat;
-grid-column: 2/6;
-grid-row: 2/4;
+grid-column: 3/5;
+grid-row: 2/3;
 display: flex;
 justify-content: center;
 align-content: center;
 `
-// export const HeadLine = styled.h2`
-//   grid-column: 3;
-//   grid-row: 2;
-//   display: flex;
-//   align-self: flex-end;
-//   justify-self: center;
-//   color: rgb(68, 63, 63);
-//   font-size: 42px;
-//   margin: 0px;
-// `
+export const Img = styled.img `
+  height: 100%;
+  width: 100%;
+`
+export const HeadLine = styled.h2 `
+  grid-column: 3;
+  grid-row: 2;
+  display: flex;
+  align-self: flex-end;
+  justify-self: center;
+  color: rgb(68, 63, 63);
+  font-size: 42px;
+  margin: 0px;
+`
 
 // export const Paragraph = styled.p`
 // grid-column: 2/5;
