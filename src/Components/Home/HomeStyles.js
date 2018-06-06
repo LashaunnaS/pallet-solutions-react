0d72059e719f6301a3b6eaa3c4599ae4367f9fd5
Lashaunna Samuels <shaunna191@hotmail.com>
2018-06-06 17:11:29 -0400

    Refactored grid layout for Home section

--- src/Components/Home/HomeStyles.js
+++ src/Components/Home/HomeStyles.js
@@ -10,6 +10,26 @@ export const Home = styled.main `
   display: grid;
   grid-template-columns: 34% 33% 32%;
   grid-template-rows: 25% 20% 20% auto;
+
+  @media(max-width: 1024px){
+    grid-template-columns: 32% 33% 32%;
+    grid-template-rows: 25% 25% 25% 25%;
+  }
+
+  @media(max-width: 768px){
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 25% 20% 25% 30%;
+  }
+
+  @media(max-width: 375px){
+    grid-template-columns: 10% 1fr 10%;
+    grid-template-rows: 17% 23% 32% 29%;
+  }
+
+  @media(max-width: 320px){
+    grid-template-columns: 10% 1fr 10%;
+    grid-template-rows: 10% 25% 36% 29%;
+  }
 `;
 
 export const MainTitle = styled.h2 `
@@ -45,13 +65,23 @@ export const Slogan = styled.p `
   display: flex;
   align-self: flex-start;
   justify-content: center;
-  padding: 0 5%;
+  margin: 0 2em;
   color: #fff;
   font-size: 30px;
-  line-height: 40px;
+  line-height: 1.4;
   padding: 20px 0 50px;
   font-weight: 300;
   text-align: center;
+
+  @media(max-width: 375px){
+    font-size: 25px;
+    line-height: 1.3;
+    padding-top: 2em;
+    }
+
+  @media(max-width: 320px){
+    font-size: 25px;
+    line-height: 1.3;}
 `
 export const Button = styled.button `
   grid-column: 2;
@@ -82,7 +112,6 @@ export const Button = styled.button `
     color: #dd5638;
   }
 
-
 @media (max-width: 816px) {
   width: 50%;
 }
@@ -96,8 +125,14 @@ export const Button = styled.button `
   width: 70%;
 }
 
-@media (max-width: 556px) {
+@media (max-width: 477px) {
   margin-top: 5em;
   width: 100%;
+  grid-column: 2;
+}
+
+@media (max-width: 375px) {
+  width: 70%;
+  grid-column: 2;
 }
 `
